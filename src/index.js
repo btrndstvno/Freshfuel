68341e7512792c61b892630c905eac11687125c0
Betrand <c14230184@john.petra.ac.id>
2025-04-07 16:25:39 +0700

    Fix issues and prepare for deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Freshfuel">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/customize" element={<CustomizePage />} />
